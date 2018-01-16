cda8c4a2a112424d75a8095828347a4c94c4681d
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2018-01-16 11:40:40 -0500

    Initial commit 2

---
+++ src/com/neo/hangman/Hangman.java
@@ -0,0 +1,22 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+package com.neo.hangman;
+
+import acm.program.ConsoleProgram;
+
+public class Hangman extends ConsoleProgram {
+
+    public static void main(String[] args) {
+        new Hangman().start(args);
+    }
+
+    public void run() {
+		/* You fill this in */
+	}
+
+}
